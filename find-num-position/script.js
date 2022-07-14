ca2a534097a645d34a23ca231c6e4477829ecf11
leowilson <leobca2018@gmail.com>
2022-07-14 10:33:01 +0530

    add-folder.

---
+++ find-num-position/script.js
@@ -0,0 +1,18 @@
+// find array num index position
+/* exmple :var a = [2,4,5,3,21]; */
+// create array
+var a = [];
+var numRange = parseInt(prompt('How many Numbers you Want?'));
+for (i = 0; i < numRange; i++){
+  var number = parseInt(prompt('Enter the Array Elements'));
+  a.push(number);
+}
+console.log(a);
+//find position
+var store = parseInt(prompt('What Number Position you Want:'));
+for ( i = 0; i < a.length;i++){
+if ( store == a[i]){
+ var currentData = a.indexOf(a[i]);
+}
+}
+console.log('Your Number Position is: ' ,currentData);
